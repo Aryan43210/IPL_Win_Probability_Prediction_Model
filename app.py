1a25dbce45067045d34bedb0829aae4eac20e32b
aryan <fronted@aryan.com>
2026-06-11 00:31:19 +0530

    Show pickle loading error

--- app.py
+++ app.py
@@ -17,8 +17,11 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
        'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
        'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
        'Sharjah', 'Mohali', 'Bengaluru']
-
-pipe = pickle.load(open('pipe.pkl','rb'))
+try:
+    pipe = pickle.load(open('pipe.pkl','rb'))
+except Exception as e:
+    st.error(str(e))
+    raise
 st.title('IPL Win Predictor')
 
 col1, col2 = st.columns(2)
@@ -42,11 +45,14 @@ with col5:
     wickets = st.number_input('Wickets out')
 
 if st.button('Predict Probability'):
-    runs_left = target - score
-    balls_left = 120 - (overs*6)
-    wickets = 10 - wickets
-    crr = score/overs
-    rrr = (runs_left*6)/balls_left
+    if overs == 0:
+        st.error("Overs completed must be greater than 0")
+    else:
+        runs_left = target - score
+        balls_left = 120 - (overs*6)
+        wickets = 10 - wickets
+        crr = score/overs
+        rrr = (runs_left*6)/balls_left
 
     input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
 
